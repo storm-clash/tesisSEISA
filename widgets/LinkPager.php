9ada53d325e524b4cbc6fe8eb1093923c1b5b923
storm-clash <58226583+storm-clash@users.noreply.github.com>
2023-09-18 11:11:40 -0700

    fin

---
+++ widgets/LinkPager.php
@@ -0,0 +1,28 @@
+<div class="col-md-6">
+    <div class="title">
+        <h3>Pagination</h3>
+    </div>
+    <ul class="pagination pagination-primary">
+        <!--
+                    color-classes: "pagination-primary", "pagination-info", "pagination-success", "pagination-warning", "pagination-danger"
+                -->
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">1</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">...</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">5</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">6</a></li>
+        <li class="active page-item"><a href="javascript:void(0);" class="page-link">7</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">8</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">9</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">...</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">12</a></li>
+    </ul>
+    <ul class="pagination pagination-info">
+        <li class="page-item"><a href="javascript:void(0);" class="page-link"> prev</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">1</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">2</a></li>
+        <li class="active page-item"><a href="javascript:void(0);" class="page-link">3</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">4</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">5</a></li>
+        <li class="page-item"><a href="javascript:void(0);" class="page-link">next </a></li>
+    </ul>
+</div>
